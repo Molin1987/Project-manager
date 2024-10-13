0b0fb0b6a2ac2fe7bab28589f61f04340034d10a
ArslanBatt <148847954+ArslanBatt@users.noreply.github.com>
2024-10-13 17:36:48 +0500

    13.10 v.1

--- Project-manager/project-manager-front/script/script.js
+++ Project-manager/project-manager-front/script/script.js
@@ -1,15 +1,26 @@
-$(document).ready(()=>{
-    $.ajax({
-        url: "http://project-manager-back/",
-        method : "GET",
-        success: (response)=>{
-            $("#result").html(response);
-        },
-        error: ()=>{
-            console.log("Error in fetching data");
-            $("#result").html(
-                "An error occurred while fetching data. Please try again later"
-            );
-        },
-    });
-});
+// $(document).ready(()=>{
+//     $.ajax({
+//         url: "http://project-manager-back/",
+//         method : "GET",
+//         success: (response)=>{
+//             $("#result").html(response);
+//         },
+//         error: ()=>{
+//             console.log("Error in fetching data");
+//             $("#result").html(
+//                 "An error occurred while fetching data. Please try again later"
+//             );
+//         },
+//     });
+// });
+
+        // добавления проектов и задач будет здесь
+        $('#projectForm').submit(function (event) {
+            event.preventDefault();
+            // Логика для добавления проекта
+        });
+
+        $('#taskForm').submit(function (event) {
+            event.preventDefault();
+            // Логика для добавления задачи
+        });
